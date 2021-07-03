bdea3af582961d951c4ee77aa3baf6ce1eda1ada
ArtsiomMatlash <boomixguy198573@gmail.com>
2021-07-04 02:15:05 +0300

    new update

--- import_modules/path.js
+++ import_modules/path.js
@@ -1,2 +1,5 @@
 const path = require("path");
 console.log ("Название файла: ",path.basename(__filename));
+console.log ("Название дирректории: ", path.dirname(__filename));
+console.log ("Расширение файла: ", path.extname(__filename));
+console.log ('Parse: ', path.parse(__filename));
